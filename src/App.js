7610b4196f12077d0177cbe2855d82a29b88d561
mikinty <youmichaelc@gmail.com>
2021-05-08 15:48:07 -0400

    Basic boilerplate for the react webpack sass setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+/**
+ * This is the highest level of the web app.
+ */
+class App extends React.Component {
+  render() {
+    return (
+      <div id="App">Hello World</div>
+    );
+  }
+}
+
+export default App;
