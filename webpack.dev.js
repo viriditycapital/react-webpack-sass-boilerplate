53f41607cc841a8a144fdc034e4f1f2b61927ad4
mikinty <youmichaelc@gmail.com>
2021-09-11 23:00:48 -0400

    Added packages needed for build
    - gh-pages
    - file-loader (load images)
    - webpack-dev-server: updated this package to the new one and fixed the
    webpack.dev.js config to support this

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,8 +5,6 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './build',
-    historyApiFallback: true,
     hot: true
   }
 });
